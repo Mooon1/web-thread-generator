5f1ee66be9060a39ea91184b40732109c3d0b5a6
Mooon1 <xenodev@outlook.de>
2021-04-16 20:13:11 +0200

    finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 let VERSIONS = ['1.16', '1.15', '1.14', '1.13', '1.12', '1.11', '1.10', '1.9', '1.8'];
 let COMMANDS = 0;
+let FEATURES = 0;
 let THREAD = {
     name: 'Some name',
     version: '1.0.0',
     description: 'Enter a plugin description.',
     supportedVersions: [
     ],
+    features: [],
     commands: []
 };
 
@@ -19,12 +21,12 @@ function load() {
             // setting a timeout
         },
     }).done(function(data) {
-        data = JSON.parse(data);
-        $("#name").val(data.data.name);
-        $("#version").val(data.data.version);
-        $("#description").val(data.data.description);
+        THREAD = JSON.parse(data).data;
+        $("#name").val(THREAD.name);
+        $("#version").val(THREAD.version);
+        $("#description").val(THREAD.description);
         for (let v in VERSIONS){
-            let supported = data.data.supportedVersions[v].supported;
+            let supported = THREAD.supportedVersions[v].supported;
 
             if("true" == supported){
                 $('#v_' + VERSIONS[v].replace(".", "_")).prop('checked', true);
@@ -32,12 +34,17 @@ function load() {
                 $('#v_' + VERSIONS[v].replace(".", "_")).prop('checked', false);
             }
 
-            $("#label_v_" + VERSIONS[v].replace(".", "_")).val(data.data.supportedVersions[v].version);
+            $("#label_v_" + VERSIONS[v].replace(".", "_")).val(THREAD.supportedVersions[v].version);
         }
 
-        for(let c in data.data.commands){
-            let cmd = data.data.commands[c];
+        for(let c in THREAD.commands){
+            let cmd = THREAD.commands[c];
             createCommandFormWithValues(cmd.command, cmd.description, cmd.permission);
+        }
+
+        for(let f in THREAD.features){
+            let featr = THREAD.features[f];
+            createFeatureFormWithValues(featr.title, featr.description);
         }
 
         finishThread(false);
@@ -57,11 +64,29 @@ function createCommandFormWithValues(command, desc, perm) {
 function createCommandForm() {
     $("#commands-form").append('<div class="col-12">\n' +
         '                                <input type="text" id="command-' + COMMANDS + '" placeholder="/example <uuid> <itemid> <amount>" style="width:100%;">\n' +
-        '                                <input type="text" id="command-permission-' + COMMANDS + '" placeholder="/example <uuid> <itemid> <amount>" style="width:100%;">\n' +
+        '                                <input type="text" id="command-permission-' + COMMANDS + '" placeholder="plugin.command.example" style="width:100%;">\n' +
         '                                <textarea id="command-desc-' + COMMANDS + '" style="width:100%;height:100px;" placeholder="Describe the usage of the commands."></textarea>\n' +
         '                            </div>');
     COMMANDS++;
     $("#commands-counter").html(COMMANDS);
+}
+
+function createFeatureForm() {
+    $("#featuers-form").append('<div class="col-12">\n' +
+        '                                <input type="text" id="feature-' + FEATURES + '" placeholder="Name your feature" style="width:100%;">\n' +
+        '                                <textarea id="feature-desc-' + FEATURES + '" style="width:100%;height:100px;" placeholder="Describe the feature."></textarea>\n' +
+        '                            </div>');
+    FEATURES++;
+    $("#features-counter").html(FEATURES);
+}
+
+function createFeatureFormWithValues(title, desc) {
+    $("#featuers-form").append('<div class="col-12">\n' +
+        '                                <input type="text" id="feature-' + FEATURES + '" value="' + title + '" style="width:100%;">\n' +
+        '                                <textarea id="feature-desc-' + FEATURES + '" style="width:100%;height:100px;">' + desc + '</textarea>\n' +
+        '                            </div>');
+    FEATURES++;
+    $("#features-counter").html(FEATURES);
 }
 
 function finishThread(withSave) {
@@ -85,6 +110,14 @@ function finishThread(withSave) {
         supportedVersionsStr += versionVal ? '<div class="col-1"><h3><span class="badge rounded-pill bg-success">' + VERSIONS[v] + '</span></h3></div>' : '<div class="col-1"><h3><span class="badge rounded-pill bg-danger">' + VERSIONS[v] + '</span></h3></div>';
     }
 
+    if(0 >= FEATURES){
+        $("#features").hide();
+    }
+
+    if(0 >= COMMANDS){
+        $("#commands").hide();
+    }
+
     let commandStr = '';
 
     for(let i = 0;i < COMMANDS;i++){
@@ -95,12 +128,32 @@ function finishThread(withSave) {
         };
 
         commandStr += ' <tr>\n' +
-        '                            <td><b>' + THREAD.commands[i].command + '</b></td>\n' +
-        '                            <td style="color:#aaa;">' + THREAD.commands[i].description + '</td>\n' +
-        '                            <td style="color:#aa8684;">' + THREAD.commands[i].permission + '</td>\n' +
-        '                        </tr>';
+            '                            <td><b>' + THREAD.commands[i].command + '</b></td>\n' +
+            '                            <td style="color:#aaa;">' + THREAD.commands[i].description + '</td>\n' +
+            '                            <td style="color:#aa8684;">' + THREAD.commands[i].permission + '</td>\n' +
+            '                        </tr>';
     }
     $("#commands-table").html(commandStr);
+
+    let featureStr = '';
+
+    for(let i = 0;i < FEATURES;i++){
+        THREAD.features[i] = {
+            "title": $("#feature-" + i).val(),
+            "description": $("#feature-desc-" + i).val(),
+            "version": (typeof THREAD.features[i] == "undefined") ? THREAD.version : THREAD.features[i].version
+        };
+
+        featureStr += '<a href="#" class="list-group-item list-group-item-action" aria-current="true">\n' +
+            '                                <div class="d-flex w-100 justify-content-between">\n' +
+            '                                    <h5 class="mb-1">' + THREAD.features[i].title + '</h5>\n' +
+            '                                    <small>VERSION ' + THREAD.features[i].version + '</small>\n' +
+            '                                </div>\n' +
+            '                                <p class="mb-1">' + THREAD.features[i].description + '</p>\n' +
+            '                            </a>';
+    }
+
+    $("#features-list").html(featureStr);
 
     $("#supported-versions").html(supportedVersionsStr);
 
