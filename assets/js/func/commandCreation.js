963d815d7e7ec59e4d0b062e56e71339bf9a060e
Mooon1 <xenodev@outlook.de>
2021-04-17 18:42:20 +0200

    adj

---
+++ assets/js/func/commandCreation.js
@@ -0,0 +1,37 @@
+function createCommandFormWithValues(command, desc, perm) {
+    $("#commands-form").append('<div class="col-12">\n' +
+        '                                <input type="text" id="command-' + COMMANDS + '" value="' + command + '" style="width:100%;">\n' +
+        '                                <input type="text" id="command-permission-' + COMMANDS + '" value="' + desc + '" style="width:100%;">\n' +
+        '                                <textarea id="command-desc-' + COMMANDS + '">' + perm + '</textarea>\n' +
+        '                            </div>');
+    COMMANDS++;
+    $("#commands-counter").html(COMMANDS);
+}
+
+function createCommandForm() {
+    $("#commands-form").append('<div class="col-12">\n' +
+        '                                <input type="text" id="command-' + COMMANDS + '" placeholder="/example <uuid> <itemid> <amount>" style="width:100%;">\n' +
+        '                                <input type="text" id="command-permission-' + COMMANDS + '" placeholder="plugin.command.example" style="width:100%;">\n' +
+        '                                <textarea id="command-desc-' + COMMANDS + '" style="width:100%;height:100px;" placeholder="Describe the usage of the commands."></textarea>\n' +
+        '                            </div>');
+    COMMANDS++;
+    $("#commands-counter").html(COMMANDS);
+}
+
+function createFeatureForm() {
+    $("#featuers-form").append('<div class="col-12">\n' +
+        '                                <input type="text" id="feature-' + FEATURES + '" placeholder="Name your feature" style="width:100%;">\n' +
+        '                                <textarea id="feature-desc-' + FEATURES + '" style="width:100%;height:100px;" placeholder="Describe the feature."></textarea>\n' +
+        '                            </div>');
+    FEATURES++;
+    $("#features-counter").html(FEATURES);
+}
+
+function createFeatureFormWithValues(title, desc) {
+    $("#featuers-form").append('<div class="col-12">\n' +
+        '                                <input type="text" id="feature-' + FEATURES + '" value="' + title + '" style="width:100%;">\n' +
+        '                                <textarea id="feature-desc-' + FEATURES + '" style="width:100%;height:100px;">' + desc + '</textarea>\n' +
+        '                            </div>');
+    FEATURES++;
+    $("#features-counter").html(FEATURES);
+}
--- assets/js/func/finishThread.js
+++ assets/js/func/finishThread.js
@@ -1,45 +1,8 @@
-function createCommandFormWithValues(command, desc, perm) {
-    $("#commands-form").append('<div class="col-12">\n' +
-        '                                <input type="text" id="command-' + COMMANDS + '" value="' + command + '" style="width:100%;">\n' +
-        '                                <input type="text" id="command-permission-' + COMMANDS + '" value="' + desc + '" style="width:100%;">\n' +
-        '                                <textarea id="command-desc-' + COMMANDS + '">' + perm + '</textarea>\n' +
-        '                            </div>');
-    COMMANDS++;
-    $("#commands-counter").html(COMMANDS);
-}
-
-function createCommandForm() {
-    $("#commands-form").append('<div class="col-12">\n' +
-        '                                <input type="text" id="command-' + COMMANDS + '" placeholder="/example <uuid> <itemid> <amount>" style="width:100%;">\n' +
-        '                                <input type="text" id="command-permission-' + COMMANDS + '" placeholder="plugin.command.example" style="width:100%;">\n' +
-        '                                <textarea id="command-desc-' + COMMANDS + '" style="width:100%;height:100px;" placeholder="Describe the usage of the commands."></textarea>\n' +
-        '                            </div>');
-    COMMANDS++;
-    $("#commands-counter").html(COMMANDS);
-}
-
-function createFeatureForm() {
-    $("#featuers-form").append('<div class="col-12">\n' +
-        '                                <input type="text" id="feature-' + FEATURES + '" placeholder="Name your feature" style="width:100%;">\n' +
-        '                                <textarea id="feature-desc-' + FEATURES + '" style="width:100%;height:100px;" placeholder="Describe the feature."></textarea>\n' +
-        '                            </div>');
-    FEATURES++;
-    $("#features-counter").html(FEATURES);
-}
-
-function createFeatureFormWithValues(title, desc) {
-    $("#featuers-form").append('<div class="col-12">\n' +
-        '                                <input type="text" id="feature-' + FEATURES + '" value="' + title + '" style="width:100%;">\n' +
-        '                                <textarea id="feature-desc-' + FEATURES + '" style="width:100%;height:100px;">' + desc + '</textarea>\n' +
-        '                            </div>');
-    FEATURES++;
-    $("#features-counter").html(FEATURES);
-}
-
 function finishThread(withDownload, asPrint) {
     asPrint = typeof asPrint != "undefined";
     THREAD.name = $("#name").val();
     $("#d-name").html('<span>' + THREAD.name + '</span>');
+    $("#icon").html(THREAD.name.substring(0, 7).toUpperCase());
     THREAD.description = $("#description").val();
     $("#d-description").html('<span>' + THREAD.description + '</span>');
     THREAD.version = $("#version").val();
